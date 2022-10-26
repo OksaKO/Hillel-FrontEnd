92a55adecb502255d376f7758b273008819bdbdf
OksaKO <oksana.korobko.11@gmail.com>
2022-10-26 14:42:24 +0300

    Homework 9

---
+++ hw9/js/app.js
@@ -0,0 +1,66 @@
+const EXCHANGE = 41;
+const DISCOUNT_STARTS_FROM = 100000;
+const DISCOUNT_STR = 10;
+const DISCOUNT = DISCOUNT_STR / 100;
+let productNumber;
+let amount;
+let selectedProduct;
+let finalPrice;
+let superFinalPrice;
+let discount;
+
+// Show products
+function showProducts() {
+  for (let i = 0; i < products.length; i++) {
+    console.log(`#${(i+1)} Name: ${products[i].name} \nPrice: ${products[i].price}`);
+  }
+}
+
+// Get product number
+function getProductNumber(){
+  do {
+    productNumber = prompt('Enter the product you wanna buy:');
+    productNumber--;
+  } while(productNumber < 0 || productNumber > products.length - 1 || isNaN(productNumber));
+  selectedProduct = products[productNumber];
+}
+
+// Get amount of products to be bought
+function getAmountProducts(){
+  do {
+    amount = parseInt(prompt('Enter the amount of products you wanna buy:'));
+  } while(amount <= 0 || isNaN(amount));
+}
+
+// Calculate the initial price
+function initialPrice (){
+  finalPrice = selectedProduct.price * amount;
+}
+
+// Check if discount needed
+function checkDiscount(){
+  if (finalPrice * EXCHANGE > DISCOUNT_STARTS_FROM) {
+    console.log(`Congrats! You got discount ${DISCOUNT_STR}%:`);
+    discount =  finalPrice * DISCOUNT;
+  } else {
+    discount = 0;
+  }
+}
+
+// Calculate final price
+function calcFinalPrice(){
+   superFinalPrice = finalPrice - discount;
+   console.log('Please pay $' + superFinalPrice);
+}
+
+function shop(){
+  showProducts();
+  getProductNumber();
+  getAmountProducts();
+  initialPrice();
+  checkDiscount();
+  calcFinalPrice();  
+}
+
+shop();
+
---
+++ hw9/js/data.js
@@ -0,0 +1,22 @@
+const products = [
+    {
+      name: 'AKM',
+      price: 700,
+    },
+    {
+      name: 'AR-15',
+      price: 1500,
+    },
+    {
+      name: 'Savage 110',
+      price: 500,
+    },
+    {
+      name: 'Mossberg 500',
+      price: 1000
+    },
+    {
+      name: 'Glock 17',
+      price: 600,
+    },
+  ];
